a13ae32f3ee153ada4b9b7cf8e561c71fbfe8efd
Sean Pollock <seanpollock3344@gmail.com>
2017-09-24 19:29:18 -0700

    script to create users and save to disk in testrpc format

--- scripts/createAccounts.js
+++ scripts/createAccounts.js
@@ -3,17 +3,32 @@ const fs = require('fs')
 const { privateToAddress } = require('ethereumjs-util')
 
 const params = { keyBytes: 32, ivBytes: 16 }
-let accountNum = 10
+const balance = '0x28DF9A72FDE228000'
+let acntNum = 10
 let accounts = []
 
 async function createAccounts() {
   try {
-    const pk = keythereum.create(params).privateKey.toString('hex').replace(/^/, '0x')
-    console.log('pk', pk)
-    return pk
+    const privKey = keythereum.create(params).privateKey.toString('hex').replace(/^/, '0x')
+    const pubKey = privateToAddress(privKey).toString('hex').replace(/^/, '0x')
+    accounts.push({ secretKey: privKey, publicKey: pubKey, balance })
+    acntNum--
+    if (acntNum <= 1) return true
+    return createAccounts()
   } catch (err) {
     console.log('### error in createAccounts', err)
   }
 }
 
-createAccounts().then(() => {})
+async function writeAccounts() {
+  return new Promise((res, rej) => {
+    fs.writeFile('../conf/accounts.json', JSON.stringify(accounts), 'utf8', err => {
+      if (err) rej(err)
+      res(true)
+    })
+  })
+}
+
+createAccounts().then(() => writeAccounts())
+.then(() => console.log('### wrote accounts to ./conf/accounts.json'))
+.catch(err => console.log('### error in account creation', err))
