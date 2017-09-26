49623f065bce02eb5943347c03da34add619fa02
Sean Pollock <seanpollock3344@gmail.com>
2017-09-26 09:02:28 -0700

    adding getConbase method

---
+++ test/utils.js
@@ -0,0 +1,29 @@
+const ElasticToken = artifacts.require('./ElasticToken.sol')
+const provider = require('ethjs-provider-http')
+const rpc = require('ethjs-rpc')
+const eth = new rpc(new provider('http://localhost:8547'))
+
+function getContract() {
+  return new Promise((res, rej) => {
+    ElasticToken.deployed()
+    .then(inst => res(inst))
+    .catch(err => rej(err))
+  })
+}
+
+async function getCoinbase() {
+  try {
+    const accounts = await eth.sendAsync({
+      method: 'eth_accounts',
+      params: []
+    })
+    return accounts[0]
+  } catch (err) {
+    console.log('### error in getCoinbase', err)
+  }
+}
+
+module.exports = {
+  getContract,
+  getCoinbase
+}
