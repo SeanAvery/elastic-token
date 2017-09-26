03a286bf60333750af54cdcebd0b6ae87bfc9a66
Sean Pollock <seanpollock3344@gmail.com>
2017-09-26 09:03:27 -0700

    intitial balance test passing

--- test/elasticToken.js
+++ test/elasticToken.js
@@ -1,6 +1,7 @@
 const ElasticToken = artifacts.require('./ElasticToken.sol')
 const {
   getContract,
+  getCoinbase
 } = require('./utils')
 
 const tokenParams =  [ '0x77161ac6576e2870f64ca147de120f0ba8a66255', 1e9, 0, 'Gold', 'GLD' ]
@@ -22,6 +23,17 @@ contract('ElasticToken', (accounts) => {
         assert.equal(ticker, tokenParams[4])
       } catch (err) {
         console.log('### error in test1', err)
+      }
+    })
+
+    it('Should initially assign the total supply to msg.sender', async () => {
+      try {
+        const elasticToken = await getContract()
+        const deployer = await getCoinbase()
+        const balance = await elasticToken.balances.call(deployer)
+        assert.equal(balance, tokenParmas[1])
+      } catch (err) {
+        console.log('### error in test 2', err)
       }
     })
   })
